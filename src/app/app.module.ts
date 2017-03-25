5c0210a0d25d2e28ff41fc5a446e3b862b408b8d
Andrew McConville <amcconville@acculynx.com>
2017-03-25 07:43:21 -0500

    stubs out services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { StackService } from './services/stack.service';
+import { FirebaseService } from './services/firebase.service';
+
 import { AppComponent } from './app.component';
 import { GuessListComponent } from './guess-list/guess-list.component';
 import { QuestionDetailComponent } from './question-detail/question-detail.component';
@@ -21,7 +24,8 @@ import { StackListComponent } from './stack-list/stack-list.component';
     HttpModule
   ],
   providers: [
-    
+    FirebaseService,
+    StackService
   ],
   bootstrap: [AppComponent]
 })

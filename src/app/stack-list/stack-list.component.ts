c920c96dcc80d5c50d9aa862985668eea36aca2b
Andrew McConville <amcconville@acculynx.com>
2017-03-25 09:19:00 -0500

    adds query and mock query for getting newest questions from stack

--- src/app/stack-list/stack-list.component.ts
+++ src/app/stack-list/stack-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StackService } from '../services/stack.service';
 
 @Component({
     moduleId: module.id,
     selector: 'stack-list',
     templateUrl: 'stack-list.component.html'
 })
-export class StackListComponent {
+export class StackListComponent implements OnInit {
+    questions: any;
+    
+    constructor(private stackService: StackService) {
 
+    }
+
+    ngOnInit() {
+        this.questions = this.stackService.getQuestionsByNewest();
+    }
 }

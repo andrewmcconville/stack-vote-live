96c38a4b12740488e6dcd95c1bc4d52a135bdff9
Andrew McConville <amcconville@acculynx.com>
2017-03-26 11:25:02 -0500

    adds comment and clean up queries

--- src/app/guess-list/guess-list.component.ts
+++ src/app/guess-list/guess-list.component.ts
@@ -9,30 +9,37 @@ import { FbQuestion } from '../questions-types/fbquestion';
     templateUrl: 'guess-list.component.html'
 })
 export class GuessListComponent implements OnInit {
-    batch: Array<any>;
+    guessList: Array<SoQuestion>;
 
-    constructor(private questionService: QuestionService) {
-
-    }
+    constructor(private questionService: QuestionService) { }
 
     ngOnInit() {
+        //subscribe to firebase questions
         this.questionService.getFirebaseQuestions().subscribe((fbQuestions: Array<FbQuestion>) => {
+            //map the firebase question id's to one big string seperated by a ; to query stack
             let stackQuery: string = fbQuestions.map((fbQuestion: FbQuestion) => fbQuestion.questionId).join(';');
-
+            //subscribe to stack questions using the joined stackQuery string
             this.questionService.getBatchStackQuestionsByIds(stackQuery).subscribe((soQuestions: Array<SoQuestion>) => {
-
-                this.batch = this.mergeQuestions(fbQuestions, soQuestions)
+                //merge the stack and firebase questions for the UI 
+                this.guessList = this.mergeQuestions(fbQuestions, soQuestions)
             });
         });
     }
 
-    mergeQuestions(fbq: Array<FbQuestion>, soq: Array<SoQuestion>): Array<any> {
-        console.log(fbq);
-        console.log(soq);
-        soq.map((soQuestion: SoQuestion) => {
-            soQuestion.totalGuesses = 5;
+    mergeQuestions(fbQuestions: Array<FbQuestion>, soQuestions: Array<SoQuestion>): Array<SoQuestion> {
+        //for each stack question
+        soQuestions.forEach((soQuestion: SoQuestion) => {
+            //compare it to each firebase question
+            fbQuestions.forEach((fbQuestion: FbQuestion) => {
+                //if the question ids match
+                if(soQuestion.question_id == fbQuestion.questionId) {
+                    //sum up the count of all the firebase answers and put it on stack questions totalGuesses
+                    soQuestion.totalGuesses = fbQuestion.answerCounts.reduce((a: number, b: number) => {
+                        return a + b
+                    });
+                }
+            });
         });
-
-        return soq
+        return soQuestions
     }
 }
--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -3,7 +3,6 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
 
 import { SoQuestion } from '../questions-types/soquestion';
 import { FbQuestion } from '../questions-types/fbquestion';
@@ -21,24 +20,16 @@ export class QuestionService {
         return this.af.database.list('/nest');
     }
 
-    getBatchStackQuestionsByIds(q: string): Observable<SoQuestion[]> {
-        return this.http
-            .get(this.getBatchQuery(q))
-            .map((response: Response) => response.json().items);
-    }
-
     getNewestStackQuestions(): Observable<SoQuestion[]> {
         return this.http
             .get(this.getNewestQuery())
             .map((response: Response) => response.json().items);
     }
 
-    getBatchQuery(q: string): string {
-        if(this.useStackOverflow) {
-            return `http://api.stackexchange.com/2.2/questions/${q}?order=desc&sort=activity&site=stackoverflow&key=96KYrlU5uoaBkT4Y9Fc1rw((`
-        } else {
-            return '/src/app/services/stack-batch-mock.json'
-        }
+    getBatchStackQuestionsByIds(queryIds: string): Observable<SoQuestion[]> {
+        return this.http
+            .get(this.getBatchQuery(queryIds))
+            .map((response: Response) => response.json().items);
     }
 
     getNewestQuery(): string {
@@ -46,6 +37,14 @@ export class QuestionService {
             return 'http://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=10&order=desc&sort=activity&accepted=True&answers=2&site=stackoverflow&filter=!*1SgQGDQ)5Y.lIuepUj09VlZ4y)max.5CM-rlGOA2&key=96KYrlU5uoaBkT4Y9Fc1rw(('
         } else {
             return '/src/app/services/stack-newest-mock.json'
+        }
+    }
+
+    getBatchQuery(queryIds: string): string {
+        if(this.useStackOverflow) {
+            return `http://api.stackexchange.com/2.2/questions/${queryIds}?order=desc&sort=activity&site=stackoverflow&key=96KYrlU5uoaBkT4Y9Fc1rw((`
+        } else {
+            return '/src/app/services/stack-batch-mock.json'
         }
     }
 }

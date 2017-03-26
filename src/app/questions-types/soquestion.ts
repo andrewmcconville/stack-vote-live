d67c81375080c41c4f265518f49f0960e4f8accf
Andrew McConville <amcconville@acculynx.com>
2017-03-26 12:09:28 -0500

    updates stack overflow question class

--- src/app/questions-types/soquestion.ts
+++ src/app/questions-types/soquestion.ts
@@ -1,10 +1,26 @@
 export interface ISoQuestion {
+    owner: {
+        reputation: number,
+        profile_image: string,
+        display_name: string,
+        link: string
+    };
+    accepted_answer_id: number;
+    answer_count: number;
     question_id: number;
     title: string;
     totalGuesses?: number;
 }
 
 export class SoQuestion implements ISoQuestion {
+    owner: {
+        reputation: number,
+        profile_image: string,
+        display_name: string,
+        link: string
+    };
+    accepted_answer_id: number;
+    answer_count: number;
     question_id: number;
     title: string;
     totalGuesses?: number;

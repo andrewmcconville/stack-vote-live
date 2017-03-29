f18c6f7a15e42fc1551baf915f8db1684a8b932f
Andrew McConville <andrew@andrewmcconville.com>
2017-03-28 21:46:09 -0500

    removes commented out stack-firebase get calls

--- src/app/question-guess/guess-list.component.ts
+++ src/app/question-guess/guess-list.component.ts
@@ -28,41 +28,7 @@ export class GuessListComponent implements OnInit {
         });
     }
 
-    // // double query, get question from firebase, then get question data from stack
-    // private getQuestions() {
-    //     //subscribe to firebase questions
-    //     this.questionService.getFirebaseQuestions().subscribe((fbQuestions: Array<FbQuestion>) => {
-    //         //map the firebase question id's to one big string seperated by a ; to query stack
-    //         let stackQuery: string = fbQuestions.map((fbQuestion: FbQuestion) => fbQuestion.questionId).join(';');
-    //         //if there are guesses
-    //         if(stackQuery) {
-    //             //subscribe to stack questions using the joined stackQuery string
-    //             this.questionService.getBatchStackQuestionsByIds(stackQuery).subscribe((soQuestions: Array<SoQuestion>) => {
-    //                 //merge the stack and firebase questions for the UI
-    //                 this.guessList = this.mergeQuestions(fbQuestions, soQuestions)
-    //             });
-    //         }
-    //     });
-    // }
-
-    // private mergeQuestions(fbQuestions: Array<FbQuestion>, soQuestions: Array<SoQuestion>): Array<SoQuestion> {
-    //     //for each stack question
-    //     soQuestions.forEach((soQuestion: SoQuestion) => {
-    //         //compare it to each firebase question
-    //         fbQuestions.forEach((fbQuestion: FbQuestion) => {
-    //             //if the question ids match
-    //             if(soQuestion.question_id === fbQuestion.questionId) {
-    //                 //sum up the count of all the firebase answers and put it on stack questions totalGuesses
-    //                 soQuestion.totalGuesses = fbQuestion.answerCounts.reduce((a: number, b: number) => { return a + b });
-    //                 //soQuestion.totalGuesses = fbQuestion.getTotalGuesses();
-    //                 soQuestion.firebaseKey = fbQuestion.$key;
-    //             }
-    //         });
-    //     });
-    //     return soQuestions
-    // }
-
     private deleteAllGuesses() {
-        this.questionService.deleteAllGuesses();
+        this.questionService.deleteAllFirebaseQuestions();
     }
 }

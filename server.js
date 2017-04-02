e8e241195152fd92f4bc3ed6e74d2e2b4779bd13
Andrew McConville <andrew@andrewmcconville.com>
2017-04-02 08:37:46 -0500

    setting up express for heroku deploy

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const compress = require('compression');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// activate gzip
+app.use(compress());
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});

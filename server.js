3afc872bb10edae4d4d32bb3f6328ad210d3c1ca
Andrew McConville <andrew@andrewmcconville.com>
2017-04-11 20:46:46 -0500

    adds serviceWorker: true

--- server.js
+++ server.js
@@ -18,15 +18,16 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
+// Forward to https
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
     }
-    next();
-  }
 }
 app.use(forceSSL());
 
@@ -35,13 +36,3 @@ app.use(forceSSL());
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-// app.use('*', function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] != 'https') {
-//         res.redirect('https://' + req.hostname + req.url);
-//     }
-//     else {
-//         res.sendFile(path.join(__dirname + '/dist/index.html'));
-//         //next();
-//     }
-// });

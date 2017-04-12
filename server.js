f76f28f610833f9f7759475a4fedcff1d18ff490
Andrew McConville <andrew@andrewmcconville.com>
2017-04-11 19:52:41 -0500

    path was commented out! puts it back

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const compress = require('compression');
 const express = require('express');
 const app = express();
-//const path = require('path');
+const path = require('path');
 
 // activate gzip
 app.use(compress());

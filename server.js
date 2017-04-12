b8276f2940ed793ff7500c4a2b309416d066b08e
Andrew McConville <andrew@andrewmcconville.com>
2017-04-11 20:19:02 -0500

    one more attempt at https redirect

--- server.js
+++ server.js
@@ -18,18 +18,30 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+app.use(forceSSL());
+
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
-
-app.use('*', function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect('https://' + req.hostname + req.url);
-    }
-    else {
-        res.sendFile(path.join(__dirname + '/dist/index.html'));
-        //next();
-    }
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+// app.use('*', function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] != 'https') {
+//         res.redirect('https://' + req.hostname + req.url);
+//     }
+//     else {
+//         res.sendFile(path.join(__dirname + '/dist/index.html'));
+//         //next();
+//     }
+// });
